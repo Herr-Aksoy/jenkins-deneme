a47751a5e50be2a5bbabbe14ff2d18b423afe7de
unknown <mat.aksoy06@gmail.com>
2023-12-25 16:39:59 +0100

    s3 bucket sistem degistirildi

--- a9-s3Bucket.tf
+++ a9-s3Bucket.tf
@@ -7,11 +7,13 @@ resource "aws_s3_bucket" "proje2blog" {
 
 resource "aws_s3_bucket" "proje2_awspublic_link" {
   bucket        = "proje2.awspublic.link"
+  # depends_on = [ aws_s3_bucket.proje2blog ]
   force_destroy = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "example_awspublic_link" {
   bucket = aws_s3_bucket.proje2_awspublic_link.id
+  # depends_on = [ aws_s3_bucket_ownership_controls.example_blog ]
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -20,6 +22,7 @@ resource "aws_s3_bucket_ownership_controls" "example_awspublic_link" {
 
 resource "aws_s3_bucket_ownership_controls" "example_blog" {
   bucket = aws_s3_bucket.proje2blog.id
+  # depends_on = [ aws_s3_bucket.proje2blog ]
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -28,6 +31,7 @@ resource "aws_s3_bucket_ownership_controls" "example_blog" {
 
 resource "aws_s3_bucket_public_access_block" "example_awspublic_link" {
   bucket = aws_s3_bucket.proje2_awspublic_link.id
+  # depends_on = [ aws_s3_bucket_ownership_controls.example_blog ]
 
   block_public_acls       = false
   block_public_policy     = false
@@ -37,6 +41,7 @@ resource "aws_s3_bucket_public_access_block" "example_awspublic_link" {
 
 resource "aws_s3_bucket_public_access_block" "example_blog" {
   bucket = aws_s3_bucket.proje2blog.id
+  # depends_on = [ aws_s3_bucket_ownership_controls.example_awspublic_link ]
 
   block_public_acls       = false
   block_public_policy     = false
@@ -46,6 +51,7 @@ resource "aws_s3_bucket_public_access_block" "example_blog" {
 
 resource "aws_s3_object" "index_awspublic_link" {
   bucket       = aws_s3_bucket.proje2_awspublic_link.id
+  depends_on = [ aws_s3_bucket_website_configuration.failover_awspublic_link ]
   key          = "index.html"
   source       = "./s3_Website/index.html"
   etag         = filemd5("./s3_Website/index.html")
@@ -55,6 +61,7 @@ resource "aws_s3_object" "index_awspublic_link" {
 
 resource "aws_s3_object" "sorry_awspublic_link" {
   bucket = aws_s3_bucket.proje2_awspublic_link.id
+  depends_on = [ aws_s3_bucket_website_configuration.failover_awspublic_link ]
   key    = "sorry.jpg"
   source = "./s3_Website/sorry.jpg"
   etag   = filemd5("./s3_Website/sorry.jpg")
@@ -63,6 +70,7 @@ resource "aws_s3_object" "sorry_awspublic_link" {
 
 resource "aws_s3_bucket_website_configuration" "failover_awspublic_link" {
   bucket = aws_s3_bucket.proje2_awspublic_link.id
+  # depends_on = [ aws_s3_bucket_ownership_controls.example_awspublic_link ]
 
   index_document {
     suffix = "index.html"

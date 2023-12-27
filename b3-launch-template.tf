0a9cae5dc349d69e40c866501b2ef57633a2ab2d
unknown <mat.aksoy06@gmail.com>
2023-12-27 14:11:09 +0100

    name degisikligi

--- b3-launch-template.tf
+++ b3-launch-template.tf
@@ -23,12 +23,13 @@ resource "aws_launch_template" "proje2_launch_template" {
   network_interfaces {
     security_groups = [aws_security_group.proje2_EC2_Sec_Group.id]
   }
+  count = 4
 
 
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "proje2_web_server"   
+      Name = "WebApp${count.index + 1}" #"proje2_web_server"   
     }
   }
 
